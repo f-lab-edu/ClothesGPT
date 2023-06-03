140b319b455f91407cf8e094fd94f226a811273c
ABlued <skwodhkswkd@naver.com>
2023-06-03 13:28:44 +0900

    슬랙에 알림이 가는지 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 
 export default function Home() {
+  const a = 1;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
