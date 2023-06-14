eb0796dbe75cbe4b28a747f22afa835a1dd08614
sungbin <sungbin3833@gmail.com>
2023-06-14 21:30:29 +0900

    [JAN-11] basic layout of app directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <main className={'main'}>
+        {children}
+      </main>
+      </body>
     </html>
   )
 }
