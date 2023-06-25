4378401cd76b42f86cee23af61e675c4163621d4
sungbin <sungbin3833@gmail.com>
2023-06-25 19:40:47 +0900

    [JAN-16] add gender at survey for seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,13 +4,30 @@ const prisma = new PrismaClient();
 
 const seed = async () => {
   const surveysData: survey[] = [
-    { question: '자신의 성별을 선택해주세요.', active: true, order: 1, id: 1 },
+    {
+      question: '자신의 성별을 선택해주세요.',
+      gender: 'all',
+      active: true,
+      order: 1,
+      id: 1,
+    },
+    {
+      question: '본인이 찾고 계신 옷의 스타일을 클릭하세요!',
+      gender: 'female',
+      active: true,
+      order: 2,
+      id: 2,
+    },
     // Add more survey data as needed
   ];
 
   const choicesData: choice[] = [
     { value: '남자', surveyId: 1, id: 1 },
     { value: '여자', surveyId: 1, id: 2 },
+    { value: '미니멀', surveyId: 2, id: 3 },
+    { value: '아메카지', surveyId: 2, id: 4 },
+    { value: '오피스', surveyId: 2, id: 5 },
+    { value: '힙합', surveyId: 2, id: 6 },
     // Add more choice data as needed
   ];
 
