2207fd14c691db04ff2bd909281e38b3a147f19f
sungbin <sungbin3833@gmail.com>
2023-07-15 01:29:54 +0900

    [JAN-19] survey page 구현

--- src/components/survey/MultipleChoices.tsx
+++ src/components/survey/MultipleChoices.tsx
@@ -2,13 +2,23 @@
 import React from 'react';
 import BlankBlock from '@/src/components/survey/BlankBlock';
 import { choice } from '@prisma/client';
+import { UseSurveyReturnType } from '@/src/components/survey/hooks/useSurvey';
+import { choice as choiceType, survey } from '.prisma/client';
 
 interface MultipleChoicesInterface {
   choices: choice[];
+  surveyHook: UseSurveyReturnType;
+  surveyId: number;
 }
 
 const MultipleChoices: React.FC<MultipleChoicesInterface> = (props) => {
-  const { choices } = props;
+  const { choices, surveyHook, surveyId } = props;
+  const isDisabled = surveyHook.isButtonDisabled(surveyId);
+  const answer = surveyHook.getUserAnswerBySurveyId(surveyId)?.choiceValue;
+  const onChoiceClick = (choice: choice) => {
+    if (isDisabled) return;
+    surveyHook.onClickSurveyChoice(choice as choiceType & { survey: survey });
+  };
   return (
     <div className="flex flex-row mb-2 flex-wrap">
       <BlankBlock />
@@ -16,15 +26,28 @@ const MultipleChoices: React.FC<MultipleChoicesInterface> = (props) => {
         if (choice.type === 'button') {
           return (
             <button
+              disabled={isDisabled}
+              onClick={() => onChoiceClick(choice)}
               key={choice.value}
-              className="mr-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+              className={
+                isDisabled
+                  ? choice.value === answer
+                    ? 'mr-2 bg-blue-500 text-white font-semibold py-2 px-4 border border-blue-500 rounded'
+                    : 'mr-2 bg-transparent text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded'
+                  : 'mr-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4' +
+                    ' border border-blue-500 hover:border-transparent rounded'
+              }
             >
               {choice.value}
             </button>
           );
         } else if (choice.type === 'image' && choice.image) {
           return (
-            <>
+            <div
+              onClick={() => onChoiceClick(choice)}
+              className="flex"
+              key={choice.value}
+            >
               {idx % 2 === 0 && idx > 1 && <BlankBlock />}
               <div key={choice.value}>
                 <img
@@ -35,7 +58,7 @@ const MultipleChoices: React.FC<MultipleChoicesInterface> = (props) => {
                 />
                 <div className="text-center">{choice.value}</div>
               </div>
-            </>
+            </div>
           );
         }
       })}
