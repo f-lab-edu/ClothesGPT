adcf5516ebc9cc216562c410df448f5fc15e3c28
Sungbin Ahn <sungbin3833@gmail.com>
2023-07-16 11:47:50 +0900

    fix lint error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,6 +7,8 @@ declare global {
 
 const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+if (process.env.NODE_ENV === 'development') {
+  global.prisma = prisma;
+}
 
 export default prisma;
