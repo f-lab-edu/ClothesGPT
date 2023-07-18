a17c7cf2dce81fd7c607cf0eb48bad5543e62066
ABlued <skwodhkswkd@naver.com>
2023-07-18 23:04:23 +0900

    chore : React Hooks test setup

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,30 @@
 module.exports = {
+  rootDir: './',
   testEnvironment: 'jsdom',
+  preset: 'ts-jest',
   // setupFiles: ['<rootDir>/__tests__/setup.js'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)'],
+  testMatch: ['**/?(*.)+(spec|test).(js|ts|tsx)'],
   clearMocks: true,
+  transformIgnorePatterns: ['<rootDir>/../node_modules/'],
   collectCoverageFrom: [
     '**/*.{ts,tsx}',
     '!**/node_modules/**',
     '!**/types/**/*',
   ],
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+    '^.+.(svg|png|jpg)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/$1',
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/modules/(.*)$': '<rootDir>/modules/$1',
+    '^@/types/(.*)$': '<rootDir>/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
   coveragePathIgnorePatterns: ['/node_modules/'],
   testPathIgnorePatterns: ['/node_modules/'],
 };
