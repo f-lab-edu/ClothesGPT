4c0a966dd5ec7d8618d7fd79bb0009f604f072ab
ABlued <skwodhkswkd@naver.com>
2023-07-19 00:32:23 +0900

    chore : delete src folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-import User from '@/src/components/User';
-
 // Prisma does not support Edge without the Data Proxy currently
 // export const runtime = 'edge'
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center">
-      <User />
-    </main>
+    <main className="relative flex min-h-screen flex-col items-center justify-center" />
   );
 }
