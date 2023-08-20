7b91745872aa6babe386b70b7e6a46de1bd90883
ABlued <skwodhkswkd@naver.com>
2023-08-20 20:29:54 +0900

    chore : vercel setting

--- prisma/migrations/20230820110705_init/migration.sql
+++ prisma/migrations/20230820110705_init/migration.sql
@@ -2,7 +2,7 @@
 CREATE TYPE "ChoiceComponentType" AS ENUM ('Chat', 'Button', 'Image', 'Color', 'Input');
 
 -- CreateEnum
-CREATE TYPE "inputType" AS ENUM ('Number');
+CREATE TYPE "InputType" AS ENUM ('Number');
 
 -- CreateTable
 CREATE TABLE "Question" (
@@ -21,29 +21,11 @@ CREATE TABLE "Choice" (
     "color" TEXT,
     "value" JSONB,
     "tag" JSONB,
-    "inputType" "inputType",
     "questionId" TEXT NOT NULL,
+    "inputType" "InputType",
 
     CONSTRAINT "Choice_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Post" (
-    "id" SERIAL NOT NULL,
-    "authorId" INTEGER NOT NULL,
-
-    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
-);
-
 -- AddForeignKey
 ALTER TABLE "Choice" ADD CONSTRAINT "Choice_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
